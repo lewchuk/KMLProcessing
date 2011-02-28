f2abc87cc683eced8f19b2a3ba674c1b55dee006
Stephen <Stephen.Lewchuk@gmail.com>
2011-02-27 17:20:09 -0800

    Program how has the capability to determine angles between points and create a histogram of the points.

--- KmlCompressor.py
+++ KmlCompressor.py
@@ -1,5 +1,6 @@
 #! /usr/bin/python
 
+import math
 import optparse
 import os
 import shutil
@@ -11,7 +12,6 @@ usage = "usage: %prog [options] source"
 parser = optparse.OptionParser(usage=usage)
 parser.add_option("-d", "--destination", dest="destination", help="output file for compression, defaults to <source>.cmp.kml")
 parser.add_option("-n", "--number", dest="number", type="int", default="950", help="number of datapoints to compress to")
-parser.add_option("-a", "--angle", dest="angle", type="float", default=30, help="maximum angle in degrees for a datapoint to be removed")
 parser.add_option("-f", "--force", dest="force", action="store_true", help="overwrite existing destination files")
 
 def extractCoordinates(geometryNode):
@@ -35,7 +35,75 @@ GEOMETRIES = { "Point":extractPoint,
                "LineString":extractLineString,
                "LinearRing":extractLinearRing }
 
+class KMLPath:
+    def __init__(self,geometry):
+        self._geo = geometry
+        self._coords = extractCoordinates(geometry)
+    
+    def __len__(self):
+        return len(self._coords)
+
+    def filterIdentical(self):
+        """ Filters the coordinates accociated with this geometry
+        entry for identical lat/long coordinates.  It ignores altitude
+        at the moment.
+
+        Returns:
+            difference -- The number of data points removed
+        """
+        newcoords = []
+        
+        for i in range(1,len(self._coords)):
+            p1 = coordinates[i-1].split(',')
+            p2 = coordinates[i].split(',')
+            if p1[0] != p2[0] or p1[1] != p2[1]:
+                filtered.append(coordinates[i])
+
+        difference = len(self._coords) - len(newcoords)
+        self._coords = newcoords
+
+        return difference
+  
+    def angleProfile(self, bucketSize):
+        """ Generates a histogram of angles in the coordinates.
+        For every point [1,n-1] determines the difference of the angle
+        between it and the previous point and between the previous point
+        and the following point.
+
+        Parameters:
+            bucketSize -- The size of bucket to use for the histogram
+                          buckets are [0,bucketSize), [bucketSize,bucketSize*2)...
+
+        Returns:
+            histogram -- A dictionary of bucket to frequency.
+        """
+    
+        histogram = {}
+        for i in range(1,len(self._coords) -1):
+            angle = determineAngle(self._coords[i-1],
+                                   self._coords[i],
+                                   self._coords[i+1])
+
+            bucket = int(angle/bucketSize) * bucketSize
+            if bucket not in histogram:
+                histogram[bucket] = 0
+            histogram[bucket] = histogram[bucket] + 1
+
+        return histogram
+
+
 def extractGeometries(pm):
+    """ Extracts from a <Placemark> any compressable geometries
+    and the size of any non-compressable geometries
+
+    Parameters:
+        pm -- A DOM Element preresenting a Placemark
+
+    Returns:
+        count -- Number of datapoints for all non-compressable geometries
+        kmlPaths -- A list of KMLPath objects which can be compressed
+    """
+
     count = 0
     tocompress = []
 
@@ -45,7 +113,8 @@ def extractGeometries(pm):
             count += size
             tocompress.extend(geos)
 
-    return (count, tocompress)
+    kmlPaths = [KMLPath(x) for x in tocompress]
+    return (count, kmlPaths)
 
 def extractCompressable(placemarks):
     count = 0
@@ -58,11 +127,64 @@ def extractCompressable(placemarks):
 
     return (count, tocompress)
 
-def compressGeometries(geometries, limit, angle):
-    pass
+def determineBearing(p1, p2):
+    """ Determines the bearing between p1 and p2 along the great
+    circle which connects them.
 
+    p1 -- Point 1 - "long,lat[,alt]"
+    p2 -- Point 2 - "long,lat[,alt]"
 
-def compress(source, dest, limit, angle):
+    Source for mathematics: http://www.movable-type.co.uk/scripts/latlong.html
+    """
+    
+    point1 = p1.split(',')
+    point2 = p2.split(',')
+    
+    dlon = math.radians(float(point2[0]) - float(point1[0]))
+
+    lat1 = math.radians(float(point1[1]))
+    lat2 = math.radians(float(point2[1]))
+
+    y = math.sin(dlon) * math.cos(lat2) 
+    x = (math.cos(lat1) * math.sin(lat2) - math.sin(lat1) * math.cos(lat2) *
+         math.cos(dlon))
+    bearing = math.atan2(y, x)
+
+    if bearing < 0:
+        bearing += 2*math.pi
+
+    return bearing
+
+def determineAngle(p1, p2, p3):
+    """ This function used three strings of "lat,long[,alt]" to determine
+    the angle between the line p1p2 and p1p3.  The angle is based on the
+    initial bearings along the great circles connecting p1 and p2 and
+    p1 and p3. Because the angle is based on the difference of two angles
+    the absolute value is returned.
+
+    Source for mathematics: http://www.movable-type.co.uk/scripts/latlong.html
+
+    p1 -- point 1 - "long,lat[,alt]"
+    p2 -- point 2 - "long,lat[,alt]"
+    p3 -- point 3 - "long,lat[,alt]"
+    """
+
+    bearing1 = determineBearing(p1,p2);
+    bearing2 = determineBearing(p1,p3);
+
+    angle = bearing2 - bearing1
+
+    return math.degrees(abs(angle))
+
+def compressGeometries(geometries, limit):
+    print "Filtering identical lat/long points"
+
+    identical = 0
+
+    for geo in geometries:
+        print "\n".join(["%s\t%s" % x for x in sorted(geo.angleProfile(5).items())])
+
+def compress(source, dest, limit):
     doc = minidom.parse(source)
 
     placemarks = doc.getElementsByTagName("Placemark")
@@ -71,30 +193,11 @@ def compress(source, dest, limit, angle):
 
     remainingDP = limit - size
 
-    
+    compressGeometries(geometries, remainingDP) 
 
     destfile = open(dest, 'w')
 
     doc.writexml(destfile)
-
-#    geometries = [pm.getGeometry() for pm in kml.getPlacemarks()]
-
-#    (count, tocompress) = extractLineStrings(geometries)
-    
-#    limit -= count
-
- #   existing = [len(x.Coordinates) for x in tocompress]
-
-  #  total = sum(existing)
-
-   # counts = [x/total*limit for x in existing]
-    
-#    for aCount, geo in zip(counts, tocompress):
-#        compressCoordinates(geo.Coordinates, aCount, angle)
-        
-#    kml.write()
-
-    
 
 if __name__ == "__main__":
     (options, args) = parser.parse_args();
@@ -114,7 +217,6 @@ if __name__ == "__main__":
         dest = "%s.cmp.kml" % source
 
     limit = options.number
-    angle = options.angle
 
     if os.path.exists(dest):
         if options.force:
@@ -123,5 +225,5 @@ if __name__ == "__main__":
             print "Destination: %s already exists" % dest
             exit()
 
-    compress(source, dest, limit, angle)
+    compress(source, dest, limit)
 
